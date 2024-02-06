aa674fca20b64fd82e70209f983100fb4e5cc974
Aprendiz <Aprendiz@BOGDFPCGMF4142.FormacionSena.RED>
2024-02-06 17:20:15 -0500

    primeras entidades

---
+++ app/entities.py
@@ -0,0 +1,9 @@
+class Carro():
+    placa = "SDF 456"
+    tipo_de_vehiculo = "Camion"
+    
+class Cliente():
+    nombre = "Pablo Rivas Alfonso"
+    documento_identidad = 1116542549
+    contacto = 3133585900
+    
